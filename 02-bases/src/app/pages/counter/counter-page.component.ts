b6b6c549903d81c0e35c6b0602e3b8ac2260ff83
e.gomez <e.gomez@garsa.com>
2025-09-04 10:57:47 +0200

    Component con html y css separado

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component( {
-    template: `
-        <h1>Counter: {{ counter }}</h1>
-        <button (click)="increaseBy(1)">+1</button>
-    `
+    templateUrl: './counter-page.component.html',
+    styleUrl: './counter-page.component.css',
 })
 export class CounterPageComponent {
     counter = 10;
 
     increaseBy( value: number ): void {
         this.counter += value;
+    }
+
+    resetCounter(): void {
+        this.counter = 10;
     }
 }
