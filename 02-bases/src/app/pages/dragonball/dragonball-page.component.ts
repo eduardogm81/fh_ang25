d9bcb540ffc5a8d5cd5b256104da5485f623c955
e.gomez <e.gomez@garsa.com>
2025-09-10 12:04:51 +0200

    @if

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -20,7 +20,8 @@ export class DragonballPageComponent {
     characters = signal<Character[]>([
         { id: 1, name: 'Goku', power: 9001 },
         { id: 2, name: 'Vegeta', power: 9000 },
-        { id: 3, name: 'Piccolo', power: 3000 }
+        { id: 3, name: 'Piccolo', power: 3000 },
+        { id: 4, name: 'Yamcha', power: 500 },
     ])
 
     powerClasses = computed( () => {
