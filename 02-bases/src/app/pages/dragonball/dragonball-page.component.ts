59eb525e328f8b36eede74aa9db7284c71fe763d
e.gomez <e.gomez@garsa.com>
2025-09-10 11:45:03 +0200

    @for

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+    id: number;
+    name: string;
+    power: number;
+}
 
 @Component( {
     selector: 'app-dragonball',
@@ -7,5 +13,11 @@ import { Component } from '@angular/core';
     styleUrl: './dragonball-page.component.css'
 } )
 export class DragonballPageComponent {
+
+    characters = signal<Character[]>([
+        { id: 1, name: 'Goku', power: 9001 },
+        { id: 2, name: 'Vegeta', power: 8000 },
+        { id: 3, name: 'Piccolo', power: 3000 }
+    ])
 
 }
