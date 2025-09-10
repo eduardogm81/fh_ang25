abd57547dabae13f28e2c00900cd20a3cc2a9e59
e.gomez <e.gomez@garsa.com>
2025-09-10 16:46:27 +0200

    Servicios

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
-import { Character } from '../../interfaces/character.interface';
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
+import { DragonballService } from '../../services/dragonball.service';
 
 
 @Component( {
@@ -14,28 +14,6 @@ import { CharacterAddComponent } from '../../components/dragonball/character-add
 } )
 export class DragonballSuperPageComponent {
 
-    name = signal('');
-    power = signal(0);
+    public dragonballService = inject(DragonballService);
 
-    characters = signal<Character[]>([
-        { id: 1, name: 'Goku', power: 9001 },
-        { id: 2, name: 'Vegeta', power: 9000 },
-        /*{ id: 3, name: 'Piccolo', power: 3000 },
-        { id: 4, name: 'Yamcha', power: 500 },*/
-    ])
-
-    powerClasses = computed( () => {
-        return {
-            'text-danger': true
-        }
-    } );
-
-    addCharacter(character: Character) {
-        this.characters.update( list => [...list, character] );
-    }
-
-    resetFields() {
-        this.name.set('');
-        this.power.set(0);
-    }
 }
