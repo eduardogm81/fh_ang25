5e27ed65f3e0778d0c7fc18d3693ac72617beacf
e.gomez <e.gomez@garsa.com>
2025-09-10 15:39:02 +0200

    Signal Inputs

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,14 +1,13 @@
 import { Component, computed, signal } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
+import { Character } from '../../interfaces/character.interface';
 
-interface Character {
-    id: number;
-    name: string;
-    power: number;
-}
 
 @Component( {
     selector: 'dragonball-super',
+    imports: [
+        CharacterListComponent
+    ],
     templateUrl: './dragonball-super-page.component.html'
 } )
 export class DragonballSuperPageComponent {
@@ -19,8 +18,8 @@ export class DragonballSuperPageComponent {
     characters = signal<Character[]>([
         { id: 1, name: 'Goku', power: 9001 },
         { id: 2, name: 'Vegeta', power: 9000 },
-        { id: 3, name: 'Piccolo', power: 3000 },
-        { id: 4, name: 'Yamcha', power: 500 },
+        /*{ id: 3, name: 'Piccolo', power: 3000 },
+        { id: 4, name: 'Yamcha', power: 500 },*/
     ])
 
     powerClasses = computed( () => {
--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -9,9 +9,6 @@ interface Character {
 
 @Component( {
     selector: 'app-dragonball',
-    imports: [
-        NgClass
-    ],
     templateUrl: './dragonball-page.component.html'
 } )
 export class DragonballPageComponent {
@@ -20,7 +17,7 @@ export class DragonballPageComponent {
     power = signal(0);
 
     characters = signal<Character[]>([
-        { id: 1, name: 'Goku', power: 9001 },
+        { id: 1, name: 'Goku', power: 9002 },
         /*{ id: 2, name: 'Vegeta', power: 9000 },
         { id: 3, name: 'Piccolo', power: 3000 },
         { id: 4, name: 'Yamcha', power: 500 },*/
