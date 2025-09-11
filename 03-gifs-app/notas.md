97b8a4ad42aeb74bd809b967304ac51eee0d2119
e.gomez <e.gomez@garsa.com>
2025-09-11 11:39:50 +0200

    Añadir rutas y rutas hijas

---
+++ 03-gifs-app/notas.md
@@ -0,0 +1,4 @@
+Ejemplo de generar una componente sin tests ni CSS:
+````
+     ng g c gifs/pages/search --skip-tests --style=none
+````
---
+++ 03-gifs-app/src/app/gifs/pages/dasboard-page/dasboard-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component( {
+    selector: 'app-dasboard-page',
+    imports: [
+        RouterOutlet
+    ],
+    templateUrl: './dasboard-page.component.html'
+} )
+export default class DasboardPageComponent {
+
+}
