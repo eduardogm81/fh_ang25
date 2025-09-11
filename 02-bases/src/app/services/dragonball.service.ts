9dff3ff7a5790486f5258628c73eafdb053560c5
e.gomez <e.gomez@garsa.com>
2025-09-11 08:17:22 +0200

    recuperar de localstorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,12 +1,19 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+
+const loadFromLocalStorage = (): Character[] => {
+    const characters = localStorage.getItem('characters');
+    return characters ? JSON.parse(characters) : [];
+}
+
 @Injectable( { providedIn: 'root' } )
 export class DragonballService {
-    characters = signal<Character[]>( [
+    characters = signal<Character[]>(loadFromLocalStorage());
+    /*characters = signal<Character[]>( [
         { id: 1, name: 'Goku', power: 9001 },
         { id: 2, name: 'Vegeta', power: 9000 },
-    ] )
+    ] )*/
 
     saveToLocalStorage = effect( () => {
         localStorage.setItem('characters', JSON.stringify( this.characters() ) );
