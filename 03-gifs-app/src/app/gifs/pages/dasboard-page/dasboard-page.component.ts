0a641cde5ff74c85548f0bd0ce7f4154b0fe25b3
e.gomez <e.gomez@garsa.com>
2025-09-11 11:56:12 +0200

    descomponer el menú en componentes

--- 03-gifs-app/src/app/gifs/pages/dasboard-page/dasboard-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/dasboard-page/dasboard-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component( {
     selector: 'app-dasboard-page',
     imports: [
-        RouterOutlet
+        RouterOutlet,
+        SideMenuComponent
     ],
     templateUrl: './dasboard-page.component.html'
 } )
